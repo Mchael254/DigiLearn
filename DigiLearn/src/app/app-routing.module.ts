8d2218a096c0416f7350cc2117fdca37418450eb
Mchael254 <owiti.otieno@s.karu.ac.ke>
2025-01-19 03:00:42 +0300

    admin user login set up

--- DigiLearn/src/app/app-routing.module.ts
+++ DigiLearn/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',redirectTo:'/user',pathMatch:'full'},
+  { path:'user',component: UserComponent},
+  { path:'login',component: LoginComponent},
+  { path:'admin',component: AdminComponent},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- DigiLearn/src/app/app.module.ts
+++ DigiLearn/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
